6bd87cdf5c5d9f2761e4835c6c1eebded1a1eaee
Fastering <73803835+Fastering18@users.noreply.github.com>
2021-07-05 20:45:00 +0700

    gblk library source files

---
+++ pkgbuild.sh
@@ -0,0 +1,13 @@
+command -v pkg >/dev/null 2>&1 || { echo >&2 "I require foo but it's not installed.  Aborting."; exit 1; }
+#!/bin/bash
+
+if ! command -v pkg &> /dev/null
+then
+    echo "`pkg` NPM package belum terinstall, menjalankan `npm install pkg -g`"
+    npm install -g pkg
+fi
+echo "Terdapat `pkg` package, melanjutkan"
+
+pkg .
+
+ping -n 8 127.0.0.1 >nul
